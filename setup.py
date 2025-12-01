2f2f76af1ce71226ef07565b5c45b016b5a94317
thorinaboenke <thorina-boenke@web.de>
2025-12-01 14:34:50 +0100

    naming in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='attackmate-client',
+    name='attackmate_client',
     version='1.0.0',
     description='A command-line client for remote execution of AttackMate playbooks.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Thorina Boenke',
     url='https://github.com/ait-testbed/attackmate-client',
-    py_modules=['client'],
+    py_modules=['attackmate_client'],
     install_requires=[
         'httpx',
         'pyyaml',
